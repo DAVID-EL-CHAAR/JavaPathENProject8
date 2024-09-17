944efb467d9a3e6b604038638ebc940258932b7a
DAVID-EL-CHAAR <80713536+DAVID-EL-CHAAR@users.noreply.github.com>
2024-09-17 17:34:26 +0200

    Update TestRewardsService.java

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestRewardsService.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestRewardsService.java
@@ -39,31 +39,7 @@ public class TestRewardsService {
 		tourGuideService.tracker.stopTracking();
 		assertTrue(userRewards.size() == 1);
 	}
-	/*@Test
-	public void userGetRewards() {
-	    GpsUtil gpsUtil = new GpsUtil();
-	    RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-
-	    InternalTestHelper.setInternalUserNumber(0);
-	    TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-
-	    User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
-	    Attraction attraction = gpsUtil.getAttractions().get(0);
-	    user.addToVisitedLocations(new VisitedLocation(user.getUserId(), attraction, new Date()));
-
-	    // Déclencher le calcul des récompenses de manière asynchrone
-	    CompletableFuture<Void> future = rewardsService.calculateRewards(user);
-
-	    // Attendre la fin du calcul des récompenses
-	    future.join();
-
-	    // Après avoir attendu, obtenir les récompenses de l'utilisateur
-	    List<UserReward> userRewards = user.getUserRewards();
-	    tourGuideService.tracker.stopTracking();
-
-	    // Vérifier que l'utilisateur a bien une récompense
-	    assertTrue(userRewards.size() == 1);
-	}*/
+	
 
 
 	@Test
@@ -74,22 +50,7 @@ public class TestRewardsService {
 		assertTrue(rewardsService.isWithinAttractionProximity(attraction, attraction));
 	}
 
-	//@Disabled // Needs fixed - can throw ConcurrentModificationException
- /*	@Test
-	public void nearAllAttractions() {
-		GpsUtil gpsUtil = new GpsUtil();
-		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-		rewardsService.setProximityBuffer(Integer.MAX_VALUE);
-
-		InternalTestHelper.setInternalUserNumber(1);
-		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-
-		rewardsService.calculateRewards(tourGuideService.getAllUsers().get(0));
-		List<UserReward> userRewards = tourGuideService.getUserRewards(tourGuideService.getAllUsers().get(0));
-		tourGuideService.tracker.stopTracking();
-
-		assertEquals(gpsUtil.getAttractions().size(), userRewards.size());
-	}*/
+	
 	
 	@Test
 	public void nearAllAttractions() {
